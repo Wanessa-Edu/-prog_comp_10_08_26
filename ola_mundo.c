8ef926eb1bb127ffdd8774cf0d1cd267d0c0c2a7
odozinha bala tensa <wanessa160401@edu.unipar.br>
2026-08-10 21:14:41 -0300

    Add files via upload

---
+++ ola_mundo.c
@@ -0,0 +1,9 @@
+#include<stdio.h> // BIBLIOTECA
+
+int main() // FUNCAO PRINCIPAL
+{
+    
+    printf("Ola Mundo C UNIPAR 2026/2");
+
+    return 0; // RETORNO - s.0. DEU TUDO CERTO
+}
---
+++ variavel_char.c
@@ -0,0 +1,42 @@
+#include<stdio.h> // BIBLIOTECA
+
+int main()  // FUNCAO PRINCIPAL
+{
+    // VAR
+    char caracter; //%c char caracter 'a';
+    char nome[20], produto [50]; // %s nome = 'Maria'
+    int ano_atual = 2026, ano_nasc, idade, qtd; // %d dia = 10
+    float vlr_unitario, total; // %f total = 1234.67 %,2f
+
+    //  INPUT
+    printf("Digite uma letra: ");
+    scanf("%c", &caracter);
+
+    printf("Qual o seu nome: ");
+    scanf("%s", &nome);
+
+    printf("Qual o ano do seu nascimento: ");
+    scanf("%d", &ano_nasc);
+
+    printf("Produto: ");
+    scanf("%s", &produto);
+
+    printf("Quantidade: ");
+    scanf("%d", &qtd);
+    
+    printf("Valor Unitario: R$ ");
+    scanf("%f", &vlr_unitario);
+
+    // PROCESSAMENTO
+    idade = ano_atual - ano_nasc;
+    total = qnt * vlr_unitario
+
+    // RETURN
+    printf("\nEu digitei a letra %c", caracter);
+    printf("\nE me chamo %s da Unipar", nome);
+    printf("\nMeu nome eh %s e tenho %d anos.", nome, idade);
+    printf("\nComprei qtd %d - %s de R$ %.2f, total R$ %.2f ", qtd, produto vlr_unitario);
+
+    return 0;  // RETORNO 0 - EXECUTOU COM SUCESSO O PROGRAMA
+
+}
